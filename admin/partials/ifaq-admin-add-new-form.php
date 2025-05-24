24353112f1461623bd35e1f290fb0c39e718de61
Fahad <fahad.abdullah1280@gmail.com>
2025-05-25 00:30:37 +0600

    Add status in the form'

--- admin/partials/ifaq-admin-add-new-form.php
+++ admin/partials/ifaq-admin-add-new-form.php
@@ -7,21 +7,31 @@
 
             <table class="form-table">
                 <tr>
-                    <th scope="row"><label for="faq_question">Question</label></th>
+                    <th scope="row"><label for="faq_question">Question<span class="ifaq_required">*</span></label></th>
                     <td>
                         <textarea name="question" id="faq_question" rows="3" required></textarea>
                     </td>
                 </tr>
                 <tr>
-                    <th scope="row"><label for="faq_answer">Answer</label></th>
+                    <th scope="row"><label for="faq_answer">Answer<span class="ifaq_required">*</span></label></th>
                     <td>
                         <textarea name="answer" id="faq_answer" rows="5" required></textarea>
                     </td>
                 </tr>
                 <tr>
-                    <th scope="row"><label for="faq_category">Category</label></th>
+                    <th scope="row"><label for="faq_category">Category<span class="ifaq_required">*</span></label></th>
                     <td>
                         <input type="text" name="category" id="faq_category" value="general" required>
+                    </td>
+                </tr>
+
+                <tr>
+                    <th scope="row"><label for="faq_status">Status<span class="ifaq_required">*</span></label></th>
+                    <td>
+                        <select name="status" id="faq_status" required>
+                            <option value="active" selected>Active</option>
+                            <option value="deactive">Deactive</option>
+                        </select>
                     </td>
                 </tr>
             </table>
