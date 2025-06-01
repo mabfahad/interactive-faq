d598e9cda86a39ae0f6b83c05d3da864a31e1c1f
Fahad <fahad.abdullah1280@gmail.com>
2025-06-02 02:35:12 +0600

    Add comments for all the function written on the Ifaq_Activator class

--- includes/class-ifaq-activator.php
+++ includes/class-ifaq-activator.php
@@ -11,28 +11,32 @@
  */
 
 /**
- * Fired during plugin activation.
+ * Class responsible for tasks to run during plugin activation.
  *
- * This class defines all code necessary to run during the plugin's activation.
+ * This class defines all the code necessary to set up the plugin's environment
+ * when it is activated, such as creating required database tables.
  *
  * @since      1.0.0
  * @package    Ifaq
  * @subpackage Ifaq/includes
  * @author     Md Abdullah Al Fahad <mabf.fahad@gmail.com>
  */
-class Ifaq_Activator {
+class Ifaq_Activator
+{
 
-	/**
-	 * Short Description. (use period)
-	 *
-	 * Long Description.
-	 *
-	 * @since    1.0.0
-	 */
-	public function activate() {
+    /**
+     * Executes activation logic for the plugin.
+     *
+     * Initializes the database handler and creates necessary tables.
+     *
+     * @return void
+     * @since 1.0.0
+     */
+    public function activate()
+    {
         global $wpdb;
         $ifaq_db = new Ifaq_DB($wpdb);
         $ifaq_db->create_tables_at_activation();
-	}
+    }
 
 }
